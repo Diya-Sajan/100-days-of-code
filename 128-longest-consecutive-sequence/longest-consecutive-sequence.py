b48f1322b562259bedb28a05c8d575d0ec0d0eee
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-10-08 02:14:55 +0530

    Time: 360 ms (49.79%) | Memory: 30.5 MB (27.57%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -3,12 +3,13 @@ class Solution(object):
         nums = set(nums)
         nums = sorted(nums)
         nums = list(nums)
-        high = [1]
-        count=1
+        
         if (len(nums)==0):
             return 0
-
-
+            
+        high = [1]
+        count=1
+        
         for i in range(0,len(nums)-1):
             if (nums[i]+1== nums[i+1]):
                 count= count+1            
@@ -18,10 +19,7 @@ class Solution(object):
             if (i==len(nums)-2):
                 high.append(count)
         
-        high = set(high)
-        high = sorted(high)
-        
-        return high[-1] 
+        return max(high) 
 
 
         
