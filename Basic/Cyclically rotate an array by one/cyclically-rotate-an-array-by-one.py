bb626a10edd78e6afe6e541e2094232f579113c8
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-11-30 22:21:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Cyclically rotate an array by one/cyclically-rotate-an-array-by-one.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+def rotate( arr, n):
+
+    for i in range(0,n+1):
+        x=arr.pop()
+        arr.insert(0,x)
+        
+
+    return arr
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
