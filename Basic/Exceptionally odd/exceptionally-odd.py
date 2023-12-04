2149932364e48a4caa16f3e6ae698da40ababf5d
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-12-05 02:58:08 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Exceptionally odd/exceptionally-odd.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def getOddOccurrence(self, arr, n):
+        f = {}
+        for i in arr:
+            if i not in f:
+                f[i] = 1
+            else:
+                f[i]+= 1
+        
+        for j in f:
+            if f[j] % 2 != 0:
+                return j
+        
+        return -1
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getOddOccurrence(arr, n)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
