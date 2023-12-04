7bf89e1bf085c8f4ca54646698387854c14cd8af
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-12-04 17:24:05 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find the smallest and second smallest element in an array/find-the-smallest-and-second-smallest-element-in-an-array.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+def minAnd2ndMin( a, n):
+    arr = list(set(a))
+    arr.sort()
+    if len(arr)<=1:
+        return [-1,-1]
+    return arr[:2]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = minAnd2ndMin(a, n)
+        if product[0]==-1:
+            print(product[0])
+        else:
+            print(product[0], end=" ")
+            print(product[1])
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
