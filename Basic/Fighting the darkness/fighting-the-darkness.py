686c56b5a4ee1b27c579cb0e61d356a05e70e2e6
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-12-07 23:24:07 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Fighting the darkness/fighting-the-darkness.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def maxDays(self, arr, n):
+        return max(arr)
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__': 
+    t=int(input())
+    for _ in range(0,t):
+        n=int(input())
+        arr = list(map(int,input().split()))
+        ob = Solution()
+        print(ob.maxDays(arr,n))
+# } Driver Code Ends
