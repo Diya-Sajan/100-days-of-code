fc8900cc7ac2e02aaf7dce766de00ba346992902
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2023-12-11 03:15:47 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum product of two numbers/maximum-product-of-two-numbers.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+
+	def maxProduct(self,arr, n):
+		if n < 2 : return 0
+		arr= sorted(arr)
+		return arr[-1]*arr[-2]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.maxProduct(arr, n)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
