a005e2eb2e02f7bd8fd0b069efb06d1deee26728
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2024-02-09 11:18:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (14.93%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -7,10 +7,19 @@ public:
             return 0;
         }
         
-        int x = 1;
+        /*int x = 1;
         while(x<=n){
-            x=x*2;
+            x=x*2; 
         }
-        return (x-1)^n;
+        return (x-1)^n;*/
+        int m=n;
+        int mask=0;
+
+        while(m!=0){
+            mask=(mask<<1)|1;
+            m=m>>1;
+        }
+        int ans =(~n) & mask;
+        return ans;
     }
 };
