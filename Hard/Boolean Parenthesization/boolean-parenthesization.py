1e7652c9bc21a2cdb35d638570af0298d2115330
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2024-02-21 23:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Boolean Parenthesization/boolean-parenthesization.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+from functools import lru_cache
+
+class Solution:
+    def countWays(self, n, s):
+        mod = 1003
+        exp = s
+        @lru_cache(maxsize=None)
+        def recur(requirement, l, r):
+            if abs(l-r) == 1:
+                if exp[l] == requirement:
+                    return 1
+                else:
+                    return 0
+                
+            res = 0
+            for i in range(l, r):
+                if not exp[i].isalpha():
+                    lt = recur('T', l, i)
+                    lf = recur('F', l, i)
+                    rt = recur('T', i+1, r)
+                    rf = recur('F', i+1, r)
+                    if exp[i] == '&':
+                        if requirement == 'T':
+                            res += lt * rt
+                        else:
+                            res += (lf * rf) + (lf * rt) + (lt * rf)
+                    elif exp[i] == '|':
+                        if requirement == 'T':
+                            res += (lt * rt) + (lf * rt) + (lt * rf)
+                        else:
+                            res += lf * rf
+                    elif exp[i] == '^':
+                        if requirement == 'T':
+                            res += (lf * rt) + (lt * rf)
+                        else:
+                            res += (lf * rf) + (lt * rt)
+            
+            return res % mod
+        
+        return recur('T', 0, n) % mod
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(10**6)
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        S = input()
+        
+        ob = Solution()
+        print(ob.countWays(N, S))
+# } Driver Code Ends
