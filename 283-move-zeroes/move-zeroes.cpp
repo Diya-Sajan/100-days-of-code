a4a315dc38f17b23b3e5f7cfa868dadeffe4deae
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2024-04-14 19:24:49 +0530

    Time: 15 ms (68.17%) | Memory: 21.7 MB (12.38%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& v) {
+        int x = 0;
+        for(int i=0; i<v.size(); i++){
+            if(v[i]!=0){
+                swap(v[i],v[x++]);
+            }
+        }
+    }
+};
