93381f17ced2126c70d2eb5872ed9862467b01ce
Diya-Sajan <76006734+Diya-Sajan@users.noreply.github.com>
2024-06-11 17:45:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort/merge-sort.cpp
+++ Medium/Merge Sort/merge-sort.cpp
@@ -16,81 +16,57 @@ void printArray(int arr[], int size)
 
 
 // } Driver Code Ends
-
 class Solution
 {
     public:
-    void merge(int arr[], int l, int m, int r) {
+    void merge(int arr[], int l, int m, int r)
+    {
          // Your code here
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int L[n1];
-        int R[n2];
-        int k=l;
-        
-        //insert in L[]
-        for(int i = 0 ; i < n1 ; i++) {
-            L[i] = arr[k];
-            k++;
+         int  t[r];
+    int ti=0;
+    int left = l;
+    int right = m + 1;
+
+    while (left <= m && right <= r) {
+        if (arr[left] <= arr[right]) {
+            t[ti]=arr[left];
+            left++;
+            ti++;
+        } else {
+            t[ti]=arr[right];
+            right++;
+            ti++;
         }
-        
-        //insert in R[]
-        for(int i = 0 ; i < n2 ; i++) {
-            R[i] = arr[k];
-            k++;
-        }
-        
-        //merge both in sorted order
-        int i = 0;
-        int j = 0;
-        k = l;
-        
-        while(i<n1 && j<n2) {
-            if(L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-            
-            else {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
-        }
-          
-        //if R[] ends hence insert i till end  
-        while(i < n1) {
-            arr[k] = L[i];
-            k++;
-            i++;
-        }
-          
-        //if L[] end hance insert j till end   
-        while(j < n2) {
-            arr[k] = R[j];
-            k++;
-            j++;
-        }
-        
     }
-    
+
+    while (left <= m) {
+        t[ti]=arr[left];
+            left++;
+            ti++;
+    }
+
+    while (right <= r) {
+         t[ti]=arr[right];
+            right++;
+            ti++;
+    }
+
+    for (int i = l; i <= r; i++) {
+        arr[i] = t[i - l];
+    }
+    }
     public:
     void mergeSort(int arr[], int l, int r)
     {
-        //Basecase
-        if(l >= r) return;
-        
-        int mid = l + ( r - l ) / 2;
-        
-        mergeSort( arr, l, mid);
-        mergeSort( arr, mid + 1, r);
-        
-        merge( arr, l, mid, r);
+        //code here
+         if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
     }
 };
-
-
 
 //{ Driver Code Starts.
 
